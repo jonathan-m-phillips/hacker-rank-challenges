a5bd3cabdcd1b5965ab81fa0a104edb03f14df3d
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-16 12:07:34 -0500

    sept 16

--- src/Solution.java
+++ src/Solution.java
@@ -143,25 +143,67 @@ public class Solution {
 //--------------------------------------------------------------
 //    }
 
-    static int B;
-    static int H;
-    static boolean flag = true;
-
-    static {
-        Scanner scanner = new Scanner(System.in);
-        B = scanner.nextInt();
-        H = scanner.nextInt();
-        if (B <=0 || H <= 0) {
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-            flag = false;
-        }
-    }
+//    static int B;
+//    static int H;
+//    static boolean flag = true;
+//
+//    static {
+//        Scanner scanner = new Scanner(System.in);
+//        B = scanner.nextInt();
+//        H = scanner.nextInt();
+//        if (B <=0 || H <= 0) {
+//            System.out.println("java.lang.Exception: Breadth and height must be positive");
+//            flag = false;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        if (flag) {
+//            int area = B * H;
+//            System.out.print(area);
+//        }
+//    }
 
     public static void main(String[] args) {
-        if (flag) {
-            int area = B * H;
-            System.out.print(area);
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            String s = Integer.toString(n);
+
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
         }
+    }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
     }
 
 }
